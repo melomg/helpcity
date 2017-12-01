1166ca92796cffdc37f3924e8f61280dcc8c9033
Melih Gültekin <melih.gultekin@d-teknoloji.com.tr>
2017-12-01 08:53:24 +0300

    debug uses keystore, updates fabric, adds StrictMode

--- app/src/main/java/com/projects/melih/helpcity/HelpCityApplication.java
+++ app/src/main/java/com/projects/melih/helpcity/HelpCityApplication.java
@@ -1,8 +1,13 @@
 package com.projects.melih.helpcity;
 
 import android.app.Application;
+import android.os.StrictMode;
+
 import com.crashlytics.android.Crashlytics;
 import io.fabric.sdk.android.Fabric;
+import com.squareup.leakcanary.LeakCanary;
+
+import timber.log.Timber;
 
 /**
  * Created by melih on 27/11/2017.
@@ -12,6 +17,17 @@ public class HelpCityApplication extends Application {
 
     @Override
     public void onCreate() {
+        if (BuildConfig.STRICT_MODE) {
+            StrictMode.setThreadPolicy(new StrictMode.ThreadPolicy.Builder()
+                    .detectAll()
+                    .penaltyLog()
+                    .build());
+            StrictMode.setVmPolicy(new StrictMode.VmPolicy.Builder()
+                    .detectAll()
+                    .penaltyLog()
+                    .penaltyDeath()
+                    .build());
+        }
         super.onCreate();
 
         if (!BuildConfig.DEBUG) {
